6f7a59e7eec7f0de20c9bc2baaece8035e7abc6c
Alfonzo <thulanikgakole@gmail.com>
2026-01-08 11:25:55 +0000

    done with the navbar

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return <div>
+    <section>
+      <Navbar />
+    </section>
+    <section id="Homepage">Hero</section>
+    <section id="About">about</section>
+    <section>parallax</section>
+    <section id="Services">services</section>
+    <section id="Portfolio">portfolio</section>
+    <section>portfolilo2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">contact</section>
+    </div>
+};
+
+export default App;
